ec699850aa61f3dae01c7089c3fcc01f6160862a
Todd Waits <todd.waits@gmail.com>
2014-11-25 07:43:43 -0500

    adding some basic logging. Initial mongo integration (no interactions yet)

--- src/app/dauxer.py
+++ src/app/dauxer.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, send_file, render_template, send_from_directory, Markup
+from flask.ext.pymongo import PyMongo
 import os
 from integration import ifogbugz as fb
 
 app = Flask(__name__, static_url_path='')
 
+app.config['MONGO_HOST'] = os.environ['MONGO_PORT_27017_TCP_ADDR']
+app.config['MONGO_DBNAME'] = 'dauxer'
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
+
+    # online_users = mongo.db.users.find({'online': True})
 
     projects = None
     error = None
@@ -18,6 +24,11 @@ def index():
     elif type(tmp) == str:
         error = str
 
+    app.logger.info('Received the following projects: %r', [project['project'] for project in projects])
+
+    if error:
+        app.logger.error('Ran into the following error: ', error)
+
     return render_template('home.html', projects=projects,
                                         error=error)
 
@@ -28,13 +39,17 @@ def project(projectname):
                 "Scope", "User Characteristics", "Assumptions", 
                 "Dependencies", "Constraints", "Appendix"]
 
-
-
     project = fb.get_project_data(projectname)
+
+    app.logger.info('Working with: %r', projectname)
 
     cases = fb.get_requirements_cases(projectname)
 
+    app.logger.info('Number of requirement cases: %r', len(cases))
+
     wikis = fb.get_wiki_content(project['wiki_id'], sections) 
+
+    app.logger.info('Wiki Sections: %r', sections)
 
     return render_template('document.html', project=project,
                                             cases=cases,
